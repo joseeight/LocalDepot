6fe2cf8a925e83be142ae0c7735b6d487c4ae5dc
Jose Antonio Marquez Russo <jose@eightyearproject.com>
2014-02-25 21:07:54 -0800

    Unit tests stubs

--- specs/localdepot_test.js
+++ specs/localdepot_test.js
@@ -14,13 +14,23 @@ describe('LocalDepot', function() {
   });
 
   describe('LocalDepot.Depot', function() {
-    it('should have valid structure', function() {
-      var test = new LocalDepot.Depot(
-          'test', LocalDepot.storageType.LOCALSTORAGE);
+    var test, callback;
 
+    beforeEach(function() {
+      test = new LocalDepot.Depot(
+          'test', LocalDepot.storageType.INDEXEDDB);
+      callback = function() {};
+    });
+
+    afterEach(function() {
+      test = null;
+      callback = null;
+    });
+
+    it('should have valid structure', function() {
       expect(test.constructor).toBe(LocalDepot.Depot);
       expect(test.name).toBe('test');
-      expect(test.storageType).toBe(LocalDepot.storageType.LOCALSTORAGE);
+      expect(test.storageType).toBe(LocalDepot.storageType.INDEXEDDB);
       expect(typeof test.getItem).toBe('function');
       expect(test.getItem.length).toBe(2);
       expect(typeof test.getKeys).toBe('function');
@@ -33,6 +43,70 @@ describe('LocalDepot', function() {
       expect(test.removeItem.length).toBe(2);
       expect(typeof test.clear).toBe('function');
       expect(test.clear.length).toBe(0);
+    });
+
+    describe('LocalDepot.Depot.getItem', function() {
+      it('should try to getItem via interface', function() {
+        spyOn(LocalDepot._Depot, 'getItem');
+
+        test.getItem('test', callback);
+
+        expect(LocalDepot._Depot.getItem).toHaveBeenCalledWith(
+            test, 'test', callback);
+      });
+    });
+
+    describe('LocalDepot.Depot.getKeys', function() {
+      it('should try to getKeys via interface', function() {
+        spyOn(LocalDepot._Depot, 'getKeys');
+
+        test.getKeys(callback);
+
+        expect(LocalDepot._Depot.getKeys).toHaveBeenCalledWith(test, callback);
+      });
+    });
+
+    describe('LocalDepot.Depot.setItem', function() {
+      it('should try to setItem via interface', function() {
+        spyOn(LocalDepot._Depot, 'setItem');
+
+        test.setItem('test', 'value', callback);
+
+        expect(LocalDepot._Depot.setItem).toHaveBeenCalledWith(
+            test, 'test', 'value', callback);
+      });
+    });
+
+    describe('LocalDepot.Depot.hasItem', function() {
+      it('should try to check hasItem via interface', function() {
+        spyOn(LocalDepot._Depot, 'hasItem');
+
+        test.hasItem('test', callback);
+
+        expect(LocalDepot._Depot.hasItem).toHaveBeenCalledWith(
+            test, 'test', callback);
+      });
+    });
+
+    describe('LocalDepot.Depot.removeItem', function() {
+      it('should try to removeItem via interface', function() {
+        spyOn(LocalDepot._Depot, 'removeItem');
+
+        test.removeItem('test', callback);
+
+        expect(LocalDepot._Depot.removeItem).toHaveBeenCalledWith(
+            test, 'test', callback);
+      });
+    });
+
+    describe('LocalDepot.Depot.clear', function() {
+      it('should try to clear Depot via interface', function() {
+        spyOn(LocalDepot._Depot, 'clear');
+
+        test.clear();
+
+        expect(LocalDepot._Depot.clear).toHaveBeenCalledWith(test);
+      });
     });
   });
 
@@ -54,6 +128,46 @@ describe('LocalDepot', function() {
       expect(LocalDepot._Depot.openIndexedDb.length).toBe(3);
       expect(typeof LocalDepot._Depot.closeIndexedDb).toBe('function');
       expect(LocalDepot._Depot.closeIndexedDb.length).toBe(1);
+    });
+
+    describe('LocalDepot._Depot.getItem', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.getKeys', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.setItem', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.hasItem', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.removeItem', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.clear', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.openIndexedDb', function() {
+      it('should ', function() {
+      });
+    });
+
+    describe('LocalDepot._Depot.closeIndexedDb', function() {
+      it('should ', function() {
+      });
     });
   });
 
@@ -85,6 +199,11 @@ describe('LocalDepot', function() {
   });
 
   describe('LocalDepot._createDepot', function() {
+    it('should ', function() {
+    });
+  });
+
+  describe('LocalDepot._createIndexedDbInstance', function() {
     it('should ', function() {
     });
   });
